0197b7e02f2369214af65861b1ee7d1a202f2393
syedareehaquasar <syedareehaquasar@gmail.com>
2022-03-14 22:49:45 +0530

    pt -> mt

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -12,7 +12,7 @@ const Form = () => {
                 <input  className='p-2 pb-3 bg-gray-200 w-full text-blue-900 m-2 rounded-lg shadow outline-2 outline-blue-900' type="email" id="email" name="email" /><br />
                 <label className='m-2' >Date of Birth:</label><br />
                 <input className='p-2 pb-3 bg-gray-200 w-full text-blue-900 m-2 rounded-lg shadow outline-2 outline-blue-900'  type="date" id="dob" name="dob" /><br />
-                <input className='p-2 pt-4 m-1 bg-blue-500 w-full text-gray-100 rounded-lg shadow outline-2 outline-blue-900' type="submit" name='submit' /><br />
+                <input className='p-2 mt-4 m-1 bg-blue-500 w-full text-gray-100 rounded-lg shadow outline-2 outline-blue-900' type="submit" name='submit' /><br />
             </form>
         </>
     )
