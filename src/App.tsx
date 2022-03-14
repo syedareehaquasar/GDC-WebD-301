19cce89bcfe388fd57f6aff04697aeab76754d65
syedareehaquasar <syedareehaquasar@gmail.com>
2022-03-14 22:32:03 +0530

    imporved UI

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ function App() {
   return (
     <div className="flex h-screen bg-gray-100 items-center">
       <div className="w-3/4 p-4 mx-auto bg-white shadow-lg rounded-xl">
-        <img src={logo} alt="logo" />
-        <h1 className="text-center text-xl">Welcome to <br /> #react-typescript with #tailwindcss </h1>
-        <Form/>
+        <div className='flex flex-row items-center justify-center'>
+          <img src={logo} alt="logo" className="w-20 h-20 animate-spin" />
+          <h1 className="text-center text-xl pt-4">Welcome to <br /> #react-typescript with #tailwindcss </h1>
+        </div>
+        <Form />
       </div>
     </div>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,14 +5,14 @@ const Form = () => {
         <>
             <form>
                 <label className='m-2'>First name:</label><br />
-                <input  className='p-4 bg-gray-200 w-full text-blue-900 m-2' type="text" id="fname" name="fname" /><br />
+                <input  className='p-2 pb-3 bg-gray-200 w-full text-blue-900 m-2 rounded-lg shadow outline-2 outline-blue-900' type="text" id="fname" name="fname" /><br />
                 <label className='m-2' >Last name:</label><br />
-                <input className='p-4 bg-gray-200 w-full text-blue-900 m-2' type="text" id="lname" name="lname" /><br />
+                <input className='p-2 pb-3 bg-gray-200 w-full text-blue-900 m-2 rounded-lg shadow outline-2 outline-blue-900' type="text" id="lname" name="lname" /><br />
                 <label className='m-2' >Email:</label><br />
-                <input  className='p-4 bg-gray-200 w-full text-blue-900 m-2' type="email" id="email" name="email" /><br />
+                <input  className='p-2 pb-3 bg-gray-200 w-full text-blue-900 m-2 rounded-lg shadow outline-2 outline-blue-900' type="email" id="email" name="email" /><br />
                 <label className='m-2' >Date of Birth:</label><br />
-                <input className='p-4 bg-gray-200 w-full text-blue-900 m-2'  type="date" id="dob" name="dob" /><br />
-                <input className='p-2 m-1 bg-blue-500 w-full text-gray-100' type="submit" name='submit' /><br />
+                <input className='p-2 pb-3 bg-gray-200 w-full text-blue-900 m-2 rounded-lg shadow outline-2 outline-blue-900'  type="date" id="dob" name="dob" /><br />
+                <input className='p-2 m-1 bg-blue-500 w-full text-gray-100 rounded-lg shadow outline-2 outline-blue-900' type="submit" name='submit' /><br />
             </form>
         </>
     )
